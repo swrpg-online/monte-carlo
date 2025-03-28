e45dbfefe7322ae0df39a3799e278adcc78d7dc5
Joshua Ziggas <1485767+jziggas@users.noreply.github.com>
2025-03-28 13:29:00 -0400

    fix: update exports to properly import DiceResult from @swrpg-online/dice

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export { MonteCarlo, MonteCarloError } from "./MonteCarlo";
-export type { DicePool, DiceResult, MonteCarloResult } from "./MonteCarlo";
+export type { DicePool } from "@swrpg-online/dice";
+export type { DiceResult } from "@swrpg-online/dice/dist/types";
+export type { MonteCarloResult } from "./MonteCarlo";
