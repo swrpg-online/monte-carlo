027859d43d63efee5003f3589aaac5f30b77dc68
Joshua Ziggas <1485767+jziggas@users.noreply.github.com>
2025-04-21 16:16:26 -0400

    fix: export missing types HistogramData and DistributionAnalysis

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 export { MonteCarlo, MonteCarloError } from "./MonteCarlo";
 export type { DicePool } from "@swrpg-online/dice";
 export type { DiceResult } from "@swrpg-online/dice/dist/types";
-export type { MonteCarloResult } from "./MonteCarlo";
+export type {
+  MonteCarloResult,
+  HistogramData,
+  DistributionAnalysis,
+} from "./MonteCarlo";
